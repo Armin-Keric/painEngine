1fcdb574a49a853582f75546a200568c696b81ba
Kritoxx1 <turanturi08@gmail.com>
2026-05-02 21:04:04 +0200

    feat: add Getter and Setter in model file, and add Camera UBO in the vertex shader

--- include/pain_model.h
+++ include/pain_model.h
@@ -4,7 +4,9 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <vector>
 
 namespace Pain {
@@ -24,6 +26,18 @@ class PainModel {
     PainModel(const PainModel&) = delete;
     PainModel &operator=(const PainModel&) = delete;
 
+    glm::mat4 getTransform() const { return m_transform; }
+    void setTransformMatrix(const glm::vec3 val) { m_transform = glm::translate(glm::mat4(1.f), val); }
+    void setTransform(const glm::mat4 val) { m_transform = val; }
+    void setRotation(const glm::mat4& val) { m_rotation = val; };
+    glm::mat4 getRotation() const { return m_rotation; }
+    void setScale(const glm::vec3& val) { m_scale = glm::scale(glm::mat4(1.f), val); }
+    glm::mat4 getScale() const { return m_scale; }
+    void setModel(const glm::mat4& val) { m_model = val; }
+    glm::mat4 getModel() const { return m_model; }
+    void setPosition(const glm::vec3& val) { m_pos = val; }
+    glm::vec3 getPosition() const { return m_pos; }
+
     void bind(VkCommandBuffer cmdBuff);
     void draw(VkCommandBuffer cmdBuff);
 
@@ -34,5 +48,11 @@ class PainModel {
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
     uint32_t vertexCount;
+
+    glm::vec3 m_pos = glm::vec3(1.f);
+    glm::mat4 m_model = glm::mat4(1.f);
+    glm::mat4 m_transform = glm::mat4(1.f);
+    glm::mat4 m_scale = glm::mat4(1.f);
+    glm::mat4 m_rotation = glm::mat4(1.f);
 };
 }
--- src/shaders/default.vert
+++ src/shaders/default.vert
@@ -1,6 +1,11 @@
 #version 450
 layout(location=0) in vec2 aPos;
+layout (location = 1) uniform ubo_Camera {
+  mat4 model;
+  mat4 view;
+  mat4 proj;
+};
 
 void main() {
-  gl_Position = vec4(aPos, 0.0, 1.0);
+  gl_Position = model * view * proj * vec4(aPos, 0.0, 1.0);
 }
